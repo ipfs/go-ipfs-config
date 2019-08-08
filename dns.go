597fee01e4126d0425de7fce85a979e5b9ee8d24
Alessandro Segala <ItalyPaleAle@users.noreply.github.com>
2019-08-07 20:50:35 -0700

    gofmt

--- dns.go
+++ dns.go
@@ -1,22 +1,21 @@
 package config
 
 type DNS struct {
-    // CustomResolver: when true, use a custom DNS server specified in the config, rather than the machine's default DNS resolver
-    CustomResolver bool
+	// CustomResolver: when true, use a custom DNS server specified in the config, rather than the machine's default DNS resolver
+	CustomResolver bool
 
-    // Address is the address of the custom DNS server (e.g. 1.1.1.1)
-    Address string
+	// Address is the address of the custom DNS server (e.g. 1.1.1.1)
+	Address string
 
-    // Protocol to use: "udp" (or "" - default), "dns-over-https", "dns-over-tls"
-    // Optional: default is "" (= "udp")
-    Protocol string
+	// Protocol to use: "udp" (or "" - default), "dns-over-https", "dns-over-tls"
+	// Optional: default is "" (= "udp")
+	Protocol string
 
-    // DNSoverHTTPSHost is the value for the "Host" header used when making requests via DNS-over-HTTPS
-    // Optional: use if necessary
-    DNSoverHTTPSHost string
+	// DNSoverHTTPSHost is the value for the "Host" header used when making requests via DNS-over-HTTPS
+	// Optional: use if necessary
+	DNSoverHTTPSHost string
 
-    // Port is the port the DNS server listens to
-    // Optional: default value is 53 (udp), 443 (dns-over-https) or 853 (dns-over-tls) depending on the protocol used
-    Port uint
+	// Port is the port the DNS server listens to
+	// Optional: default value is 53 (udp), 443 (dns-over-https) or 853 (dns-over-tls) depending on the protocol used
+	Port uint
 }
-
