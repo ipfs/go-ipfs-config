104bb2cefa7fa3969ea90215b9e500899b374a3a
Petar Maymounkov <petarm@gmail.com>
2021-08-03 14:05:39 -0400

    update comment

--- internal.go
+++ internal.go
@@ -14,19 +14,32 @@ const (
 // InternalBitswap contains knobs for tuning bitswap resource utilization.
 // The knobs (below) document how their value should related to each other.
 // Whether their values should be raised or lowered should be determined
-// based on the metrics "pending_tasks" reported by bitswap.
+// based on the metrics active_tasks, pending_tasks, pending_block_tasks and active_block_tasks
+// reported by bitswap.
 //
 // The value of active_tasks is capped by EngineTaskWorkerCount.
 //
 // The value of pending_tasks is generally capped by the knobs below,
 // however its exact maximum value is hard to predict as it depends on task sizes
 // as well as number of requesting peers. However, as a rule of thumb,
-// this value should oscillate around a "typical" low value.
+// during healthy operation this value should oscillate around a "typical" low value
+// (without hitting a plateau continuously).
 //
 // If pending_tasks grows and eventually reaches a plateau,
 // while at the same time active_tasks is at its maximum,
 // the node has reached its resource limits and requests are being dropped (or not serviced).
 // Raising resource limits (using the knobs below) could help, assuming the hardware can support the new limits.
+//
+// The value of active_block_tasks is capped by EngineBlockstoreWorkerCount.
+//
+// The value of pending_block_tasks is indirectly capped by active_tasks, but cannot be predicted
+// as it depends on the number of blocks involved in a peer task which can vary.
+//
+// If the value of pending_block_tasks is observed to grow,
+// while active_block_tasks is at its maximum, there is indication that the number of
+// available block tasks is creating a bottleneck (either due to high-latency block operations,
+// or due to high number of block operations per bitswap peer task).
+// In such cases, try increasing the EngineBlockstoreWorkerCount.
 type InternalBitswap struct {
 	// number of threads sending outgoing messages.
 	// used to thottle the number of concurrent send operations.
@@ -35,7 +48,7 @@ type InternalBitswap struct {
 	// number of threads for blockstore operations.
 	// used to throttle the number of concurrent requests to the block store.
 	// this number should generally be a low multiple (e.g. 4) of the TaskWorkerCount,
-	// as any individual task requires a fixed low number of calls to the blockstore.
+	// but its optimal value can be informed by the metrics pending_block_tasks and active_block_tasks.
 	EngineBlockstoreWorkerCount int
 
 	// number of worker threads for decision engine task worker.
